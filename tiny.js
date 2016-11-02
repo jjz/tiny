2ab1de0909c44d743f88b76f18d879753c924fec
jjz <woaf1003@gmail.com>
2016-11-02 18:21:43 +0800

    upgrade tiny

--- tiny.js
+++ tiny.js
@@ -4,11 +4,25 @@ let config = require("./config");
 let program = require('commander');
 let fs = require("fs");
 let path = require("path");
+let supportFiles = ['png', 'jpg', 'jpeg'];
 
 tinify.key = config.tinykey;
 
+
 function tinifyFile(file, toFile) {
     console.log("tiny", "begin compress " + file);
+    let extname = path.extname(file);
+    let isImage = false;
+    for (let i = 0; i < supportFiles.length; i++) {
+        if (supportFiles[i] == extname) {
+            isImage = true;
+        }
+    }
+    if (!isImage) {
+        console.log(file + " is not image");
+        return;
+    }
+
     let source = tinify.fromFile(file);
     source.toFile(toFile, function (res) {
         console.log("tiny", "end compress " + file);
@@ -37,8 +51,11 @@ function tinifyDir(dir) {
 }
 program
     .version('1.0.0')
-    .option('-f,--file', "compress file")
-    .option('-d,--diretctory', "compress diretcory")
+    .option('-f,--file', "压缩文件，输入当前目录下的完成文件名")
+    .option('-d,--diretctory', "压缩目录，请输入当前目录下的目录名")
+    .option('-h,--help', function () {
+
+    })
     .parse(process.argv);
 
 if (program.file) {
@@ -47,7 +64,12 @@ if (program.file) {
 
 }
 if (program.diretctory) {
-    tinifyDir(program.args[0]);
+    let dir = program.args[0];
+    if (dir[dir.length - 1] == '/') {
+        dir = dir.substring(0, dir.length - 1);
+    }
+    console.log("dir", dir);
+    //tinifyDir(dir);
 
 
 }
