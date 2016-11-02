a5b08b2aed7caad6129c3a76e9f11ea2c36de092
jjz <woaf1003@gmail.com>
2016-11-02 18:01:27 +0800

    upgrade

--- tiny.js
+++ tiny.js
@@ -2,16 +2,37 @@
 let tinify = require("tinify");
 let config = require("./config");
 let program = require('commander');
-let fs =require("fs");
+let fs = require("fs");
+let path = require("path");
 
 tinify.key = config.tinykey;
 
 function tinifyFile(file, toFile) {
+    console.log("tiny", "begin compress " + file);
     let source = tinify.fromFile(file);
-    source.toFile(toFile);
+    source.toFile(toFile, function (res) {
+        if (res) {
+            console.log("tiny", "end compress " + file);
+        }
+    });
+
 }
 
 function tinifyDir(dir) {
+    let currentPath = process.cwd();
+    let tinyPath = currentPath + "/tiny";
+    fs.exists(tinyPath, function (exists) {
+        if (!exists) {
+            fs.mkdir(tinyPath);
+        }
+    });
+    fs.readdir(dir, function (err, files) {
+        for (let i = 0; i < files.length; i++) {
+            let basename = path.basename(files[i]);
+            tinifyFile(dir + "/" + basename, tinyPath + "/" + basename);
+
+        }
+    });
 
 
 }
